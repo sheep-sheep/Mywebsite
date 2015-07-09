fa10df05aeeaa880486630ba250c38e25b29264f
sheep-sheep <zhang.yang.frank@gmail.com>
2015-07-09 17:07:58 -0400

    Django App part4

--- Django/mysite/polls/admin.py
+++ Django/mysite/polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_filter = ['pub_date']
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
--- Django/mysite/polls/models.py
+++ Django/mysite/polls/models.py
@@ -1,7 +1,7 @@
-from django.db import models
+import datetime
 
-# Create your models here.
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -12,6 +12,10 @@ class Question(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
