9b5815d05b0327ff8e40d070837ae13e8a77330e
Yang <zhang.yang.frank@gmail.com>
2017-05-11 21:00:26 -0400

    learnt Django part2

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+	def __str__(self):
+		return self.question_text
+
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published') # The string parametar is to overite the field name
+
+class Choice(models.Model):
+	def __str__(self):
+		return self.choice_text
+
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
