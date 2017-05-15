ed2e3a4568fe42c6348f41a4a4e4fca8e1b6ea10
Yang <zhang.yang.frank@gmail.com>
2017-05-15 18:53:36 -0400

    part 5, test for model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
 class Question(models.Model):
 	def __str__(self):
@@ -10,6 +11,10 @@ class Question(models.Model):
 
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published') # The string parametar is to overite the field name
+
+	def was_published_recently(self):
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
 	def __str__(self):
